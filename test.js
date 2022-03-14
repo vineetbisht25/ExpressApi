0514344e23a9557d0363eabc814e810e694ce668
vineet_bisht <vineet@9stacks.com>
2022-03-14 11:11:15 +0530

    test 5

--- test.js
+++ test.js
@@ -1,5 +1,9 @@
-let firstName = "vineet"
-let middle = "singh"
-let last = "bisht"
+let firstName = "amit"
+let middle = "kumar"
+let lastname = "kandwal"
 
 console.log(firstName);
+console.log("get full name");
+console.log(middle);
+console.log("middle");
+
